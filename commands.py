ee6e258bf5a88d42dda4352382578991301acb16
minervatbd <minervatbd@gmail.com>
2026-01-17 13:05:00 -0500

    tweak test

--- commands.py
+++ commands.py
@@ -3,4 +3,4 @@ import utils
 """ temporary test command, move later TODO """
 async def test(cmd):
     response = 'test'.format(cmd.message.author.id)
-    return await utils.send_message(cmd.message.channel, cmd.message.author, response, mention = cmd.message.author)
+    return await utils.send_message(cmd.message.channel, response)
